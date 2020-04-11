013666fd99240e98b7b2cc195d982b46acef31e0
Jeff Wofford <github@jeffwofford.com>
2020-04-11 16:12:41 -0500

    Basic setup.

---
+++ main.lua
@@ -0,0 +1,373 @@
+-- Ludum Dare 46
+
+function lerp( a, b, alpha )
+	return a + (b-a) * alpha
+end
+
+-- Configuration
+text_scale( 1 )
+filter_mode( "Nearest" )
+
+screen_size( 240, 160 )
+
+SPRITE_SHEET_PIXELS_X = 512
+PIXELS_PER_TILE = 16
+TILES_X = SPRITE_SHEET_PIXELS_X // PIXELS_PER_TILE
+
+sprite_size( PIXELS_PER_TILE )
+
+WORLD_SIZE_PIXELS = 128 * PIXELS_PER_TILE
+
+barrel_ = 0.2
+bloom_intensity_ = 0
+bloom_contrast_ = 0
+bloom_brightness_ = 0
+burn_in_ = 0
+chromatic_aberration_ = 0
+noise_ = 0.025
+rescan_ = 0.25
+saturation_ = 1
+color_multiplied_r = 1
+color_multiplied_g = 1
+color_multiplied_b = 1
+bevel_ = 0
+
+local barrel_smoothed = barrel_
+local bloom_intensity_smoothed = bloom_intensity_
+local bloom_contrast_smoothed = bloom_contrast_
+local bloom_brightness_smoothed = bloom_brightness_
+local burn_in_smoothed = burn_in_
+local chromatic_aberration_smoothed = chromatic_aberration_
+local noise_smoothed = noise_
+local rescan_smoothed = rescan_
+local saturation_smoothed = saturation_
+local color_multiplied_r_smoothed = color_multiplied_r
+local color_multiplied_g_smoothed = color_multiplied_g
+local color_multiplied_b_smoothed = color_multiplied_b
+local bevel_smoothed = bevel_
+
+SCREEN_EFFECT_SMOOTH_FACTOR = 0.035
+
+function updateScreenParams()
+	barrel_smoothed = lerp( barrel_smoothed, barrel_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	bloom_intensity_smoothed = lerp( bloom_intensity_smoothed, bloom_intensity_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	bloom_contrast_smoothed = lerp( bloom_contrast_smoothed, bloom_contrast_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	bloom_brightness_smoothed = lerp( bloom_brightness_smoothed, bloom_brightness_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	burn_in_smoothed = lerp( burn_in_smoothed, burn_in_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	chromatic_aberration_smoothed = lerp( chromatic_aberration_smoothed, chromatic_aberration_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	noise_smoothed = lerp( noise_smoothed, noise_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	rescan_smoothed = lerp( rescan_smoothed, rescan_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	saturation_smoothed = lerp( saturation_smoothed, saturation_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	color_multiplied_r_smoothed = lerp( color_multiplied_r_smoothed, color_multiplied_r, SCREEN_EFFECT_SMOOTH_FACTOR )
+	color_multiplied_g_smoothed = lerp( color_multiplied_g_smoothed, color_multiplied_g, SCREEN_EFFECT_SMOOTH_FACTOR )
+	color_multiplied_b_smoothed = lerp( color_multiplied_b_smoothed, color_multiplied_b, SCREEN_EFFECT_SMOOTH_FACTOR )
+	bevel_smoothed = lerp( bevel_smoothed, bevel_, SCREEN_EFFECT_SMOOTH_FACTOR )
+	
+	barrel( barrel_smoothed )
+	bloom( bloom_intensity_smoothed, bloom_contrast_smoothed, bloom_brightness_smoothed )
+	burn_in( burn_in_smoothed )
+	chromatic_aberration( chromatic_aberration_smoothed )
+	noise( noise_smoothed, rescan_smoothed )
+	saturation( saturation_smoothed )
+	color_multiplied( color_multiplied_r_smoothed, color_multiplied_g_smoothed, color_multiplied_b_smoothed )
+	bevel( bevel_smoothed, 0 )
+end
+
+updateScreenParams()
+
+-- Vector
+
+--[[ 
+Vector class ported/inspired from 
+Processing (http://processing.org) 
+]]--
+vec2 = {}
+
+function vec2:new( x, y )
+  
+  x = x or 0
+  y = y or x
+  
+  local o = {
+    x = x,
+    y = y
+  }
+  
+  self.__index = self
+  return setmetatable(o, self)
+end
+
+function vec2:__add(v)
+  return vec2:new( self.x + v.x, self.y + v.y )
+end
+
+function vec2:__sub(v)
+	return vec2:new( self.x - v.x, self.y - v.y )
+end
+
+function vec2:__mul(v)
+	if v == nil then
+		trace( 'vec2:__mul called with nil operand.' )
+		return
+	end
+
+	if type( v ) == 'number' then
+		v = { x = v, y = v }
+	end
+
+	return vec2:new( self.x * v.x, self.y * v.y )
+end
+
+function vec2:__div(v)
+
+	if type( v ) == 'number' then
+		v = { x = v, y = v }
+	end
+	
+	if v.x == nil or v.x == 0 then
+		v.x = 1
+	end
+
+	if v.y == nil or v.y == 0 then
+		v.y = 1
+	end
+
+	return vec2:new( self.x / v.x, self.y / v.y )
+end
+
+function vec2:length()
+  return math.sqrt(self.x * self.x + self.y * self.y)
+end
+
+function vec2:lengthSquared()
+  return self.x * self.x + self.y * self.y
+end
+
+
+function vec2:dist(v)
+  local dx = self.x - v.x
+  local dy = self.y - v.y
+  
+  return math.sqrt(dx * dx + dy * dy)
+end
+
+function vec2:dot(v)
+  return self.x * v.x + self.y * v.y
+end
+
+function vec2:normal()
+  local m = self:length()
+  if m ~= 0 then
+    return self / m
+  else
+	return self
+  end
+end
+
+function vec2:limit(max)
+  local m = self.lengthSquared()
+  if m >= max * max then
+    return self:normal() * max
+  end
+end
+
+ function vec2:heading()
+  local angle = math.atan2(-self.y, self.x)
+  return -1 * angle
+end
+
+function vec2:rotate(theta)
+  local tempx = self.x
+  self.x = self.x * math.cos(theta) - self.y * math.sin(theta)
+  self.y = tempx * math.sin(theta) + self.y * math.cos(theta)
+end
+
+function vec2:angle_between(v1, v2)
+  if v1.x == 0 and v1.y then 
+    return 0
+  end
+  
+  if v2.x == 0 and v2.y == 0 then
+    return 0
+  end
+  
+  local dot = v1.x * v2.x + v1.y * v2.y 
+  local v1mag = math.sqrt(v1.x * v1.x + v1.y * v1.y)
+  local v2mag = math.sqrt(v2.x * v2.x + v2.y * v2.y)
+  local amt = dot / (v1mag * v2mag)
+  
+  if amt <= -1 then
+    return math.pi
+  elseif amt >= 1 then
+    return 0
+  end
+  
+  return math.acos(amt)
+end
+
+function vec2:set(x, y)
+  self.x = x
+  self.y = y
+end
+
+function vec2:equals(o)
+  o = o or {}
+  return self.x == o.x and self.y == o.y
+end
+
+function vec2:__tostring()
+  return '(' .. string.format( "%.2f", self.x ) .. ', ' .. string.format( "%.2f", self.y ) .. ')'
+end
+
+-- UTILITY
+
+function randInRange( a, b )
+	return lerp( a, b, math.random() )
+end
+
+function randomElement( tab )
+	local n = #tab
+	if n == 0 then return nil end
+
+	return tab[ math.random( 1, #tab ) ]
+end
+
+function tableString( tab )
+	local str = ''
+	for key, value in pairs( tab ) do
+		str = str .. key .. '=' .. value .. ', '
+	end
+	return str
+end
+
+function tableStringValues( tab )
+	local str = ''
+	for _, value in pairs( tab ) do
+		str = str .. value .. ' '
+	end
+	return str
+end
+
+function tableFind( tab, element )
+    for index, value in pairs(tab) do
+        if value == element then
+            return index
+        end
+	end
+	return nil
+end
+
+function tableRemoveValue( tab, element )
+
+	table.remove( tab, tableFind( tab, element ))
+end
+	
+local debugCircles = {}
+local debugMessages = {}
+
+function drawDebugCircles()
+	for _, circle in ipairs( debugCircles ) do
+		circ( circle[1].x, circle[1].y, 32 )	-- TODO!!!
+	end
+
+	debugCircles = {}
+end
+
+function drawDebug()
+	print( '', 0, 0 )
+
+	-- print( tostring( mousex ) .. ',' .. tostring( mousey ) .. '::' .. tostring( placeableRoom ))
+	-- print( tostring( world.focusX ) .. ',' .. tostring( world.focusY ))
+
+	-- print( 'actors: ' .. tostring( #actors ), 0, 0 )
+
+	for _,message in ipairs( debugMessages ) do
+		print( message )
+	end
+
+	while #debugMessages > 10 do
+		table.remove( debugMessages, 1 )
+	end
+end
+
+function spriteIndex( x, y )
+	return y * TILES_X + x
+end
+
+function worldToTile( x )
+	return math.floor( x / PIXELS_PER_TILE )
+end
+
+function debugCircle( center, radius, color )
+	table.insert( debugCircles, {center, radius, color })
+end
+
+function trace( message )
+	table.insert( debugMessages, message )
+end
+
+function length( x, y )
+	return math.sqrt( x * x + y * y )
+end
+
+function sign( x )
+	return x < 0 and -1 or ( x > 0 and 1 or 0 )
+end
+
+function signNoZero( x )
+	return x < 0 and -1 or 1
+end
+
+function clamp( x, minimum, maximum )
+	return math.min( maximum, math.max( x, minimum ))
+end
+
+function pctChance( percent )
+	return randInRange( 0, 100 ) <= percent
+end
+
+function round( x, divisor )
+	divisor = divisor or 1
+	return x // divisor * divisor
+end
+
+function quantize( x )
+	return round( x + 4, PIXELS_PER_TILE )
+end
+
+function sheet_pixels_to_sprite( x, y )
+	return ( y // PIXELS_PER_TILE ) * (SPRITE_SHEET_PIXELS_X//PIXELS_PER_TILE) + ( x // PIXELS_PER_TILE )
+end
+
+-- Objects
+
+-- GLOBALS
+
+-- TIME
+
+ticks = 0
+function now()
+	return ticks * 1 / 60.0
+end
+
+realTicks = 0
+function realNow()
+	return realTicks * 1 / 60.0
+end
+
+-- UPDATE
+
+function update()
+
+	realTicks = realTicks + 1
+end
+
+
+-- DRAW
+
+function draw()
+	-- cls( 0xff220044 )
+
+	camera( 0, 0 )
+	drawDebug()
+end
